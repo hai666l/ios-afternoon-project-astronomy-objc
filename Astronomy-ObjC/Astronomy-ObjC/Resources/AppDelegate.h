15f9c4df257ae1e7753ad805aa6e4c3859aa66ad
hai666l <0xconr@protonmail.com>
2020-08-05 20:44:26 -0500

    Created Project, Organized files

---
+++ Astronomy-ObjC/Astronomy-ObjC/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Astronomy-ObjC
+//
+//  Created by conner on 8/5/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Astronomy-ObjC/Astronomy-ObjC/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Astronomy-ObjC
+//
+//  Created by conner on 8/5/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
